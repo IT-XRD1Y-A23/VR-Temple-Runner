3f4379cf6ed9b141d868b3486592ecafb41fbe26
Vlad Lazar <lazar.vlad151@gmail.com>
2023-10-31 11:01:46 +0100

    floor with path

---
+++ Assets/PathGenerator.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathGenerator : MonoBehaviour
+{
+    public List<GameObject> pathSegments = new List<GameObject>();
+    public float segmentMoveSpeed = 5.0f; // Speed at which the segments move towards the player
+    public int numberOfSegmentsOnScreen = 5;
+    private float segmentLength;
+
+    private List<GameObject> activeSegments = new List<GameObject>();
+
+    void Start()
+    {
+        segmentLength = pathSegments[0].transform.localScale.z;
+
+        for (int i = 0; i < numberOfSegmentsOnScreen; i++)
+        {
+            SpawnSegmentAtStart();
+        }
+    }
+
+    void Update()
+    {
+        foreach (GameObject segment in activeSegments)
+        {
+            segment.transform.position -= Vector3.forward * segmentMoveSpeed * Time.deltaTime;
+        }
+
+        if (activeSegments[0].transform.position.z + segmentLength < 0)
+        {
+            SpawnSegment();
+            DeleteSegment();
+        }
+    }
+
+    void SpawnSegmentAtStart()
+    {
+        GameObject go = Instantiate(pathSegments[Random.Range(0, pathSegments.Count)]);
+        go.transform.SetParent(transform);
+        go.transform.position = Vector3.forward * (activeSegments.Count) * segmentLength;
+        activeSegments.Add(go);
+    }
+
+    void SpawnSegment()
+    {
+        GameObject go = Instantiate(pathSegments[Random.Range(0, pathSegments.Count)]);
+        go.transform.SetParent(transform);
+        go.transform.position = Vector3.forward * (activeSegments.Count - 1) * segmentLength;
+        activeSegments.Add(go);
+    }
+
+    void DeleteSegment()
+    {
+        Destroy(activeSegments[0]);
+        activeSegments.RemoveAt(0);
+    }
+}
