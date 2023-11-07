aa8b2b4205e9d7d672b2d3158abc43d3d0a59423
christopher gadgaard <Christopher.holmelund@gmail.com>
2023-11-07 11:04:56 +0100

    colliders, script update

--- Assets/Scripts/VRPlayerController.cs
+++ Assets/Scripts/VRPlayerController.cs
@@ -4,34 +4,15 @@ using UnityEngine.XR;
 
 public class VRPlayerController : MonoBehaviour
 {
-  
-    private Transform _controllerL, _controllerR, _headset, _player;
-    public GameObject _objectControllerL, _objectControllerR, _objectHeadset, _objectPlayer;
-    private Collider PlayerCollider;
-    
 
     void Start()
     {
-        _controllerL = _objectControllerL.GetComponent<Transform>();
-        _controllerR = _objectControllerL.GetComponent<Transform>();
-        _headset = _objectHeadset.GetComponent<Transform>();
-        _player = _objectPlayer.GetComponent<Transform>();
+
     }
 
     void Update()
     {
       
-    }
-
-    void OnCollisionEnter(Collision collision)
-    {
-        
-        // Handle collision with an obstacle
-        if (collision.gameObject.CompareTag("Obstacle"))
-        {
-            // Game over or damage player
-            Debug.Log("Collided with obstacle!");
-        }
     }
     
     void OnTriggerEnter(Collider PlayerCollider)
